71e2d50f72354202e4061081245e5c96ad0c708f
Allan Ferreira <allanferreira2001@hotmail.com>
2023-08-14 14:48:35 -0300

    pagina de listas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-void main() async {
 
+void main() {
   runApp(const MyApp());
 }
 
@@ -12,64 +12,123 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-  
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const List(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-  
-      _counter++;
-    });
-  }
+class List extends StatelessWidget {
+  const List({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-   
-        title: Text(widget.title),
+      appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(60),
+          child: InkWell(
+              onTap: () {},
+              child: AppBar(
+                backgroundColor: Colors.deepPurple,
+                title: const Text('List'),
+              ))),
+      body: ListView.builder(
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return Container(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: SizedBox(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ConstrainedBox(
+                    constraints:
+                        const BoxConstraints(minHeight: 120, maxWidth: 96),
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 10, bottom: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 20, top: 20),
+                    child: const Text("teste"),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
       ),
-      body: Center(
- 
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      bottomSheet: SizedBox(
+        width: MediaQuery.sizeOf(context).width,
+        height: 200,
         child: Column(
-             mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 40, left: 20),
+              child: const Text("Digite seu nome para Cadastrar",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+              ),),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.only(
+                              top: 20), // add padding to adjust text
+                          isDense: true,
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                            color: Colors.black,
+                          )),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 1,
+                          )),
+                          hintText: ("Digite seu nome"),
+                          hintStyle: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                        keyboardType: TextInputType.name,
+                        initialValue: null,
+                        onChanged: (value) {},
+                      ),
+                    ),
+                    Container(
+                        padding: const EdgeInsets.only(left: 30),
+                        child: InkWell(
+                          onTap: () {},
+                          child: const Icon(
+                            Icons.send,
+                            color: Colors.blue,
+                            size: 30,
+                          ),
+                        ))
+                  ],
+                )),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
